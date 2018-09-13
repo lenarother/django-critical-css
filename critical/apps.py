f3cbbdacf6897158cbbee2c09ed4ffdd27a539cc
Magdalena Rother <magdalena.rother@moccu.com>
2018-09-13 14:51:27 +0200

    Update docu

--- critical/apps.py
+++ critical/apps.py
@@ -9,7 +9,6 @@ class CriticalConfig(AppConfig):
     verbose_name = _('CriticalCSS')
 
     def ready(self):
-        # Signals for handling critical css for cms pages
-        # are installed only if django-cms is present.
         if django_cms_is_present():
+            # Include signals for refreshing critical css on page publish
             import critical.signals.handlers  # noqa
