def1c042c81ccd58b84425b9c46364c0f4cb982b
Magdalena Rother <mrother@magdalenas-mbp.localdomain>
2020-08-26 06:16:52 +0200

    Enable ignoring url query strin

--- tests/test_templatetags.py
+++ tests/test_templatetags.py
@@ -55,9 +55,77 @@ class TestCriticalTags:
 
     @mock.patch('critical.templatetags.critical_tags.calculate_critical_css')
     @mock.patch('critical.templatetags.critical_tags.use_critical_css_for_request')
+    def test_critical_object_created_with_query_string(
+            self, use_critical_mock, critical_task_mock, rf, settings):
+        settings.CRITICAL_CSS_IGNORE_QUERY_STRING = False
+        use_critical_mock.return_value = True
+        critical_task_mock.delay.return_value = 'foo bar bazz'
+
+        request = rf.get('/?p=1')
+        context = Context({'request': request})
+        template_to_render = Template(
+            '{% load critical_tags %}' '{% critical_css "css/styles.css" %}'
+        )
+        template_to_render.render(context)
+
+        request = rf.get('/?p=2')
+        context = Context({'request': request})
+        template_to_render = Template(
+            '{% load critical_tags %}' '{% critical_css "css/styles.css" %}'
+        )
+        template_to_render.render(context)
+
+        assert Critical.objects.filter(url='/').count() == 0
+        assert Critical.objects.filter(url='/?p=1').count() == 1
+        assert Critical.objects.filter(url='/?p=2').count() == 1
+
+    @mock.patch('critical.templatetags.critical_tags.calculate_critical_css')
+    @mock.patch('critical.templatetags.critical_tags.use_critical_css_for_request')
+    def test_critical_object_created_ignore_query_string(
+            self, use_critical_mock, critical_task_mock, rf, settings):
+        settings.CRITICAL_CSS_IGNORE_QUERY_STRING = True
+
+        request = rf.get('/?p=1')
+        context = Context({'request': request})
+        template_to_render = Template(
+            '{% load critical_tags %}' '{% critical_css "css/styles.css" %}'
+        )
+        template_to_render.render(context)
+
+        request = rf.get('/?p=2')
+        context = Context({'request': request})
+        template_to_render = Template(
+            '{% load critical_tags %}' '{% critical_css "css/styles.css" %}'
+        )
+        template_to_render.render(context)
+
+        assert Critical.objects.filter(url='/').count() == 1
+        assert Critical.objects.filter(url='/?p=1').count() == 0
+        assert Critical.objects.filter(url='/?p=2').count() == 0
+
+    @mock.patch('critical.templatetags.critical_tags.calculate_critical_css')
+    @mock.patch('critical.templatetags.critical_tags.use_critical_css_for_request')
     def test_success(self, use_critical_mock, critical_task_mock, rf):
         use_critical_mock.return_value = True
         request = rf.get('/')
+        CriticalFactory.create(url='/', path='/static/css/styles.css', css='foo bar')
+
+        context = Context({'request': request})
+        template_to_render = Template(
+            '{% load critical_tags %}' '{% critical_css "css/styles.css" %}'
+        )
+
+        rendered_template = template_to_render.render(context)
+        assert '<style type="text/css">foo bar</style>' in rendered_template
+        assert critical_task_mock.delay.call_count == 0
+
+    @mock.patch('critical.templatetags.critical_tags.calculate_critical_css')
+    @mock.patch('critical.templatetags.critical_tags.use_critical_css_for_request')
+    def test_success_ignore_query_string(
+            self, use_critical_mock, critical_task_mock, rf, settings):
+        settings.CRITICAL_CSS_IGNORE_QUERY_STRING = True
+        use_critical_mock.return_value = True
+        request = rf.get('/?p=3')
         CriticalFactory.create(url='/', path='/static/css/styles.css', css='foo bar')
 
         context = Context({'request': request})
